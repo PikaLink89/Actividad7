6abaf00d660777a763ca27c5ec2171766944371b
Cristian <cristian_sm@outlook.com>
2018-11-19 23:28:19 +0100

    cambio de las 23.28

--- src/com/itt/arte/Espectador.java
+++ src/com/itt/arte/Espectador.java
@@ -3,15 +3,14 @@ package com.itt.arte;
 public class Espectador {
 	// HOLAAAAAAAAAAAAA desde mi casa by Miguel
 	private String texto;
-<<<<<<< HEAD
+
 	private hola2;
 	private hola20000;
 	private hola239032023;
 	//prueba holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafdasfdsfdfsa
-=======
+
 	// HOla
 	
 	// HOla hola hola
->>>>>>> branch 'master' of https://github.com/PikaLink89/Actividad7.git
-	
+
 }
