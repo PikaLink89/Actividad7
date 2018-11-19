215dbab532ce89202e7ce2624460eac65dd7f5da
Juan Antonio <Juan Antonio@WSJUAN>
2018-11-20 00:21:53 +0100

    Clases Sala y Teatro

---
+++ src/com/itt/arte/Sala.java
@@ -0,0 +1,11 @@
+package com.itt.arte;
+
+public abstract class Sala {
+	public abstract String verProgramcion();
+	public abstract String verLocalidades();
+	public abstract String verLocalidadesOcupadas();
+	public abstract String venderLocalidad(int fila, int butaca, Espectador e);
+	public abstract String cancelarLocalidad(int fila, int butaca);
+	public abstract String consultarLocalidad(int fila, int butaca);
+	public abstract double calcularRecaudacion();
+}
