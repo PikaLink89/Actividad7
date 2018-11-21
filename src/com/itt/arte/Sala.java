7951659c3ae1834b2ae80a1eaecedca7bc85459a
Miguel Angel <Miguel@ejemplo.com>
2018-11-21 17:48:39 +0100

    Cambios en mi Sala, pero no funcionan, sigo probando.

--- src/com/itt/arte/Sala.java
+++ src/com/itt/arte/Sala.java
@@ -1,6 +1,23 @@
 package com.itt.arte;
 
-public class Sala implements Teatro {
-	
-
+public interface Sala {
+	//Ver programacion
+	int VerProgramacion
+		for (int i =0; i<matrizTeatro.length; i++) {
+			if(teatro.Equals(matrizteatro[i][1])) {
+					VerLocalidades = VerLocalidades+1;
+				}else {
+					LocalidadesOcupadas = LocalidadesOcupadas+1;
+				}	
+			}
+	String verProgramacion();
+		//system.out.println(teatro1.Programacion());
+	String verLocalidades();
+	//System.out.println("Hay "+ VerLocalidades +" Localidades");
+		//system.out.println(teatro1.Localidades());
+	String verLocalidadesOcupadas();
+//	System.out.println("Hay "+ LocalidadesOcupadas + "Localidades Ocupadas");
+		//	system.out.println(teatro1.LocalidadesOcupadas());
+	String consultarLocalidad(int fila, int butaca);
+		//	system.out.println(teatro1.consultarProgramacion());
 }
