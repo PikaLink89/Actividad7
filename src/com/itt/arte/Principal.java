49abdeb284578b1d7db5d9b598fe70d62d5a791e
Cristian <cristian_sm@outlook.com>
2018-11-24 00:51:45 +0100

    Completo

--- src/com/itt/arte/Principal.java
+++ src/com/itt/arte/Principal.java
@@ -15,40 +15,99 @@ public class Principal {
               private static Teatro teatro = new Teatro("C/ Sol, 45", 300, 2, obra, 30);
 
               public static void main(String[] args) {
-
+            	            	             	            	  
                             lector = new Scanner(System.in);
+                            
 
-                            char opc;
+
+                        char opc;
 
                             do {
 
                                          opc = mostrarMenu();
-
+                                         
                                          switch (opc) {
-
-                                                       case '1':
-
-                                                       case '2':
-
-                                                       case '3':
-
+                                         					//Se consulta programación
+                                         
+                                                       case '1': System.out.println(teatro.verProgramacion());
+                                                       			 break;
+                                                       			 
+                                                       		//Se ven todas las localidades
+                                                       case '2': teatro.verLocalidades();
+                                                                 break;
+                                                                 
+                                                            //Se muestran unicamente las ocupadas
+                                                                 
+                                                       case '3': teatro.verLocalidadesOcupadas();
+                                                       	 		 break;
+                                                       	 		 
+                                                       	 		 
+                                                       	 	//Se realiza una lectura para el método venderlocalidad
                                                        case '4':
+                                                    	   int fila = 0;
+                                                    	   int butaca = 0;
+                                                    	   Espectador e = new Espectador();
 
+                                                    	   System.out.print("¿En que fila quiere sentarse (0-4)? ");
+                                                    	   fila = lector.nextInt();
+
+                                                    	   System.out.print("¿Y en que butaca (0-9)? ");
+                                                    	   butaca = lector.nextInt();
+
+                                                    	   System.out.print("Nombre ");
+                                                    	  
+                                                    	   e.setNombre(lector.next());
+
+                                                    	   System.out.print("¿Su teléfono? ");
+                                                    	  
+                                                    	  e.setTlf(lector.next());
+
+                                                    	   System.out.print("¿Cuantos años años tiene? ");
+                                                    	   
+                                                    	   e.setEdad(lector.nextInt());
+
+                                                    	   System.out.println(teatro.venderLocalidad(fila, butaca, e));
+                                                    	   break;
+                                               	  
+                                                    	   
+                                                       //Se realiza una lectura para el metodo cancelarlocalidad
                                                        case '5':
+                                                    	   
+                                                    	   System.out.print("¿Fila (0-4)? ");
+                                                    	   fila = lector.nextInt();  
 
+                                                    	   System.out.print("¿Butaca (0-9)? ");
+                                                    	   butaca = lector.nextInt();
+                                                    	   System.out.print(teatro.cancelarLocalidad(fila, butaca));
+                                                    	   break;
+
+                                                    	   
+                                                    	   //Se hace una lecutra para consultarlocalidad
                                                        case '6':
+                                                    	   
+                                                    	   System.out.print("¿Fila (0-4)? ");
+                                                    	   fila = lector.nextInt();  
 
-                                                       case '7':
-
-                                                       case '8':
-
+                                                    	   System.out.print("¿Butaca (0-9)? ");
+                                                    	   butaca = lector.nextInt();
+                                                    	   teatro.consultarLocalidad(fila, butaca);
+                                                    	   break;
+                                                    	   
+                                                    	   
+                                                    	   //llama al método que calcular la recaudación
+                                                       case '7': System.out.println(teatro.calcularRecaudacion());
+                                                           break;
+                                                           
+                                                       case '8': System.out.println("Hasta pronto");
+                                                       	    break;
                                                        default:
+                                                    	   
 
                                           }
 
                               } while (opc!='8');
 
-                            lector.close();
+                              lector.close();
 
 }
 
@@ -83,7 +142,9 @@ public static char mostrarMenu() {
               opcion = lector.next();
 
               return opcion.charAt(0); // Devuelvo el primer caracter tecleado.
-
+             
            }
+
+	
 
 }
--- src/com/itt/arte/Teatro.java
+++ src/com/itt/arte/Teatro.java
@@ -22,46 +22,165 @@ public class Teatro extends Local implements Sala {
 	@Override
 	public String verProgramacion() {
 		
-		
+		return "Hoy representamos "+ getObra().getTitulo() 
+				+ ", género: "+getObra().getGenero() + ", Duracción: "
+				+ getObra().getMinutosDuracion() + "\n En "+ getDomicilio() 
+				+ ", Local de " +getMetros() + " metros, con "+ getAccesos() + " accesos\n Precio: "+getPrecio();
 	}
 
-		
+	
+
+	/*
+	 * Para este método realizamos dos bucles para la lectura del Array bidimensional, si el valor es difernte a null mostrará que está ocupada
+	 * por el contrario, si el valor es null mostrará que está libre
+	 */
 	@Override
 	public void verLocalidades() {
-		
+		for (int i = 0; i<localidades.length; i++) {
+			for (int d = 0; d<localidades[i].length; d++) {
+									
+				if (localidades[i][d] != null)  {
+					System.out.print(i+"."+d+" Ocupada ");
+				}
+				else {
+					System.out.print(i+"."+d+" Libre ");
+				}
+				
+				}
+			System.out.println("");
+		}
 		
 	}
 	
+	/*
+	 * Mismo método que verlocalidades pero esta vez solo imprime las que están diferente a null, mostrando los datos que se piden.
+	 * 
+	 */
 
 	@Override
 	public void verLocalidadesOcupadas() {
 		
+		
+		for (int i = 0; i<localidades.length; i++) {
+			for (int d = 0; d<localidades[i].length; d++) {
+				if (localidades[i][d] != null)  {
+					System.out.println(i+"."+d+" "+localidades[i][d].getNombre()+", tlf: "+localidades[i][d].getTlf()
+							+", Tipo: "+localidades[i][d].rangoEdad());
+				}
+				
+			}
+		}
+		
+		
 			
 	}
 
-
+/*
+ * En la clase principal recoge fila butaca y el espectador, calcula el precio en base la edad mediante if y muestra la localidad vendida.
+ * 
+ */
 	@Override
 	public String venderLocalidad(int fila, int butaca, Espectador e) {
+		double preciocondescuento=0;
+		if(e.rangoEdad()=="INFANTIL") {
+			preciocondescuento= (50*precio)/100;
+		}
+		else {
+			if(e.rangoEdad()=="MENOR") {
+				preciocondescuento= (20*precio)/100;
+			}
+			else {
+				if(e.rangoEdad()=="MAYOR") {
+					preciocondescuento= precio;
+				}
+				else {
+					if(e.rangoEdad()=="JUBILADO") {
+						preciocondescuento= (66*precio)/100;
+					}
+				}
+			}
+		}
+		if(localidades[fila][butaca] != null ) {
+			return "La localidad está ocupada";
 			
+		}
+		else
+		{
+			localidades[fila][butaca] = e;
+			
+			
+			return "Se ha vendido la localidad " +fila +"."+ butaca+" a "+e.getNombre() + " por "+preciocondescuento;
+			
+				}
+		
 		
 		
 	}
 	
-	
+	/*
+	 * Para cancelar una localidad se recoge la fila y butaca, se comprueba que está ocupada, recoge el nombre en una variable
+	 * llamada nombretemporal, deja la localidad con valor null y devuelve nombretemporal.
+	 * 
+	 */
 
 	@Override
 	public String cancelarLocalidad(int fila, int butaca) {
-		
+		if(localidades[fila][butaca] != null ) {
+			String nombretemporal;
+			nombretemporal = localidades[fila][butaca].getNombre();
+			localidades[fila][butaca] = null;
+			return nombretemporal+" ha cancelado su reserva";
+		}
+		else
+		{
+			return "La localidad ya estaba vacía";
+		}
 	}
 
-	
+	/*
+	 * Recoge fila y butaca e imprime lo solicitado
+	 * 
+	 */
 	
 	@Override
 	public void consultarLocalidad(int fila, int butaca) {
+		if(localidades[fila][butaca] != null ) {
+			double preciocondescuento=0;
+			
+			if(localidades[fila][butaca].rangoEdad()=="INFANTIL") {
+				preciocondescuento= (50*precio)/100;
+			}
+			else {
+				if(localidades[fila][butaca].rangoEdad()=="MENOR") {
+					preciocondescuento= (20*precio)/100;
+				}
+				else {
+					if(localidades[fila][butaca].rangoEdad()=="MAYOR") {
+						preciocondescuento= precio;
+					}
+					else {
+						if(localidades[fila][butaca].rangoEdad()=="JUBILADO") {
+							preciocondescuento= (66*precio)/100;
+						}
+					}
+				}
+			}
+			System.out.println("Localidad ocupada por " +localidades[fila][butaca].getNombre()+", tlf: "+localidades[fila][butaca].getTlf()
+					+" Tipo: "+localidades[fila][butaca].rangoEdad()+", Precio: " +preciocondescuento);
+		}
+		else{
+			System.out.println("Localidad Vacía ");
+		}
+		
+		
 		
 	}
 	
-	
+	/*
+	 * Se calcula el precio de todas las localidadeds ocupadas en base a la edad y se almacena en acumulado,
+	 * cuando termina de recorrer todo el array devuelve acumulado.
+	 * 
+	 */
 
 	@Override
 	public double calcularRecaudacion() {
