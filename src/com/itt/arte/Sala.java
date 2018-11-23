68c640c1fe5ef4f1d22ce79f8bac54c473a752b5
Miguel Angel <Miguel@ejemplo.com>
2018-11-23 23:08:57 +0100

    Actualizo vuestros metodos

--- src/com/itt/arte/Sala.java
+++ src/com/itt/arte/Sala.java
@@ -1,23 +1,11 @@
 package com.itt.arte;
 
 public interface Sala {
-	//Ver programacion
-	/*int VerProgramacion
-		for (int i =0; i<matrizTeatro.length; i++) {
-			if(teatro.Equals(matrizteatro[i][1])) {
-					VerLocalidades = VerLocalidades+1;
-				}else {
-					LocalidadesOcupadas = LocalidadesOcupadas+1;
-				}	
-			}*/
-	String verProgramacion();
-		//system.out.println(teatro1.Programacion());
-	String verLocalidades();
-	//System.out.println("Hay "+ VerLocalidades +" Localidades");
-		//system.out.println(teatro1.Localidades());
-	String verLocalidadesOcupadas();
-//	System.out.println("Hay "+ LocalidadesOcupadas + "Localidades Ocupadas");
-		//	system.out.println(teatro1.LocalidadesOcupadas());
-	String consultarLocalidad(int fila, int butaca);
-		//	system.out.println(teatro1.consultarProgramacion());
-}
+		public abstract String verProgramcion();
+		public abstract String verLocalidades();
+		public abstract String verLocalidadesOcupadas();
+		public abstract String venderLocalidad(int fila, int butaca, Espectador e);
+		public abstract String cancelarLocalidad(int fila, int butaca);
+		public abstract String consultarLocalidad(int fila, int butaca);
+		public abstract double calcularRecaudacion();
+	}
--- src/com/itt/arte/Teatro.java
+++ src/com/itt/arte/Teatro.java
@@ -1,16 +1,21 @@
 package com.itt.arte;
 
-public class Teatro extends Local implements Sala {
-	private Obra obra;
+public class Teatro extends Local implements Sala  {
 	private double precio;
-	Espectador [] [] localidades;
+	private Obra obra;
+	private Espectador localidades[][];
 	
-	public Teatro (Obra obra, double precio, Espectador[][] localidades) {
-		super();
+	 //Constructor.
+	
+	public Teatro(String domicilio, int metros, int accesos, Obra obra, double precio) {
+		super(domicilio, metros, accesos);
 		this.obra = obra;
 		this.precio = precio;
-		this.localidades = localidades;
-	}
+        this.localidades = new Espectador[5][10]; 
+
+	}	
+	
+	//Para la interface
 
 	@Override
 	public String verProgramacion() {
@@ -20,9 +25,8 @@ public class Teatro extends Local implements Sala {
 
 	@Override
 	public String verLocalidades() {
-		int contador = 0;
-		
-		return "pepe";
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -32,8 +36,91 @@ public class Teatro extends Local implements Sala {
 	}
 
 	@Override
+	public String venderLocalidad(int fila, int butaca, Espectador e) {		
+		this.localidades[fila][butaca] = e;
+		double precioEspectador;
+		precioEspectador = (e.getEdad()<=12)?this.precio*0.5:(e.getEdad()<=17)?this.precio*0.20:(e.getEdad()>=65)?this.precio*0.66:this.precio;
+		return "Se ha vendido la localidad " + fila + "." + butaca + " a " + e.getNombre() + " por " + precioEspectador + " euros.";		
+	}	
+
+
+	@Override
+	public String cancelarLocalidad(int fila, int butaca) {
+		
+		return localidades[fila][butaca].getNombre() + " " + " ha cancelado la reserva";		
+		localidades[fila][butaca] = null;		
+			
+	}
+
+	@Override
 	public String consultarLocalidad(int fila, int butaca) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public double calcularRecaudacion() {
+		double preciocondescuento =0;
+		double acumulado = 0;
+		for (int i = 0; i<localidades.length; i++) {
+			for (int j = 0; j<localidades[i].length; j++) {
+				
+				while (localidades[i][j] != null)  {
+					if(localidades[i][j].rangoEdad()=="INFANTIL") {
+						
+						preciocondescuento= (50*precio)/100;
+						acumulado = acumulado + preciocondescuento;
+					}
+					else {
+						if(localidades[i][j].rangoEdad()=="MENOR") {
+							preciocondescuento= (20*precio)/100;
+							acumulado = acumulado + preciocondescuento;
+						}
+						else {
+							if(localidades[i][j].rangoEdad()=="MAYOR") {
+								preciocondescuento= precio;
+								acumulado = acumulado + preciocondescuento;
+							}
+							else {
+								if(localidades[i][j].rangoEdad()=="JUBILADO") {
+									preciocondescuento= (66*precio)/100;
+									acumulado = acumulado + preciocondescuento;
+								}
+							}
+						}
+					}
+				}
+				
+			}
+		}
+		
+		return acumulado;
+			
+	}
+	//Getter and Setter
+	
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Obra getObra() {
+		return obra;
+	}
+
+	public void setObra(Obra obra) {
+		this.obra = obra;
+	}
+
+	public Espectador[][] getLocalidades() {
+		return localidades;
+	}
+
+	public void setLocalidades(Espectador[][] localidades) {
+		this.localidades = localidades;
+	}
+
 }
